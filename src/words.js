a5f242ab64c80443a1f24623252d29c0c3437103
adrianrivers <adrianlrivers@gmail.com>
2020-01-20 16:39:34 +0100

    Init commit, setup linting, add words

---
+++ src/words.js
@@ -0,0 +1,11 @@
+export const words = [
+  "What we really need is a council of elders",
+  "It's entirely possible",
+  "In some areas, you could kill 100 wolves a day and it wouldn't make a difference",
+  "Being stoned would make you a better NASCAR driver",
+  "I wish Hitler had succeeded in his art career instead of causing WW2",
+  "Hey Jamie, pull that up",
+  "Chimpanzee",
+  "Have you ever done DMT",
+  "SJW's",
+];
