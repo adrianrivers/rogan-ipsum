fccb2948ae646edabc722ea9e39339688f284b1f
adrianrivers <adrianlrivers@gmail.com>
2022-07-11 00:23:11 +1200

    :rocket: Remove unwanted font files

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
-import '@fontsource/open-sans'
 
 createApp(App).mount('#app')
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,ts}'],
   theme: {
-    extend: {
-      fontFamily: {
-        ...fontFamily,
-        sans: ['Open Sans', 'ui-sans-serif', 'system-ui'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
